929cab7617d3f178742e1da963aaf5241fb2d24e
donvine mugendi <donvinemugendi@gmail.com>
2023-05-21 03:24:41 +0300

    updated children color

--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,18 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childColor, setChildColor] =useState("#FFF");
+  function handleParentColor(){
+    const newRandomColor = getRandomColor()
+    const newRandomChildColor= getRandomColor()
+    setColor(newRandomColor);
+    setChildColor(newRandomChildColor);
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onChangeColor={handleParentColor} color={childColor}/>
+      <Child onChangeColor={handleParentColor} color={childColor}/>
     </div>
   );
 }
